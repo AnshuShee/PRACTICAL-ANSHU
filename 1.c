852defa6e53cfca7160915eb48e8753b42b4be8b
Anshu Shee <anshu.shee.cg@gmail.com>
2025-12-02 12:10:26 +0530

    PRACTICAL-C

---
+++ 1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+   
+    int num;
+    printf("Enter an Number: ");
+    scanf("%d", &num);
+
+    if (num <0) {
+        printf("The number is Negative\n");
+    } else if (num >0) {
+        printf("The number is Positive\n");
+    } else {
+        printf("The number is Zero\n");
+       
+    }
+    return 0;
+}
---
+++ 2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+   
+    int a=5;
+    int b=10;
+
+    int c;
+    int d;
+
+    c=b;
+    d=a;
+   
+    
+    printf("After Swapping: a = %d, b = %d\n", c, d);
+    return 0;
+}
